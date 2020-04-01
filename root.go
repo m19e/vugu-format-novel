ea0f6923f18cac3b7d380569be842c70e1790ef4
m19e <nexus.me7290@gmail.com>
2020-04-02 01:22:36 +0900

    [ADD]Copy to clipboard

--- root.go
+++ root.go
@@ -6,7 +6,10 @@ import "fmt"
 import "reflect"
 import "github.com/vugu/vugu"
 
-import "strings"
+import (
+	"strings"
+	"syscall/js"
+)
 
 type RootData struct {
 	Text   string
@@ -19,10 +22,15 @@ func (data *RootData) HandleChange(event *vugu.DOMEvent) {
 	data.Text = event.JSEvent().Get("target").Get("value").String()
 }
 
+func (data *RootData) Copy() {
+	js.Global().Get("document").Call("getElementById", "result").Call("select")
+	js.Global().Get("document").Call("execCommand", "Copy")
+	js.Global().Get("window").Call("getSelection").Call("removeAllRanges")
+}
+
 func checkBOL(s string) bool {
-	if s == "" {
-		return false
-	}
+	fmt.Println(s)
+	// if s == "" { return false }
 	if strings.Contains(checkList, s[:1]) {
 		return false
 	}
@@ -31,6 +39,7 @@ func checkBOL(s string) bool {
 
 func (data *RootData) Format() {
 	var result string
+	fmt.Printf("%#v", data.Text)
 	slice := strings.Split(data.Text, "\n")
 	for _, str := range slice {
 		if checkBOL(str) {
@@ -71,7 +80,7 @@ func (comp *Root) BuildVDOM(dataI interface{}) (vdom *vugu.VGNode, css *vugu.VGN
 				parent := n
 				n = &vugu.VGNode{Type: vugu.VGNodeType(1), Data: "\n            ", DataAtom: vugu.VGAtom(0), Namespace: "", Attr: []vugu.VGAttribute(nil)}
 				parent.AppendChild(n)
-				n = &vugu.VGNode{Type: vugu.VGNodeType(3), Data: "textarea", DataAtom: vugu.VGAtom(217608), Namespace: "", Attr: []vugu.VGAttribute{vugu.VGAttribute{Namespace: "", Key: "class", Val: "form-control"}, vugu.VGAttribute{Namespace: "", Key: "placeholder", Val: "変換したいテキストを入力"}, vugu.VGAttribute{Namespace: "", Key: "style", Val: "height:40vh; resize:none;"}}}
+				n = &vugu.VGNode{Type: vugu.VGNodeType(3), Data: "textarea", DataAtom: vugu.VGAtom(217608), Namespace: "", Attr: []vugu.VGAttribute{vugu.VGAttribute{Namespace: "", Key: "id", Val: "text"}, vugu.VGAttribute{Namespace: "", Key: "class", Val: "form-control"}, vugu.VGAttribute{Namespace: "", Key: "placeholder", Val: "変換したいテキストを入力"}, vugu.VGAttribute{Namespace: "", Key: "style", Val: "height:35vh; resize:none;"}}}
 				parent.AppendChild(n)
 				// @change = { data.HandleChange(event) }
 				{
@@ -125,10 +134,35 @@ func (comp *Root) BuildVDOM(dataI interface{}) (vdom *vugu.VGNode, css *vugu.VGN
 				parent := n
 				n = &vugu.VGNode{Type: vugu.VGNodeType(1), Data: "\n            ", DataAtom: vugu.VGAtom(0), Namespace: "", Attr: []vugu.VGAttribute(nil)}
 				parent.AppendChild(n)
-				n = &vugu.VGNode{Type: vugu.VGNodeType(3), Data: "textarea", DataAtom: vugu.VGAtom(217608), Namespace: "", Attr: []vugu.VGAttribute{vugu.VGAttribute{Namespace: "", Key: "class", Val: "form-control"}, vugu.VGAttribute{Namespace: "", Key: "style", Val: "height:40vh; resize:none;"}}}
+				n = &vugu.VGNode{Type: vugu.VGNodeType(3), Data: "textarea", DataAtom: vugu.VGAtom(217608), Namespace: "", Attr: []vugu.VGAttribute{vugu.VGAttribute{Namespace: "", Key: "id", Val: "result"}, vugu.VGAttribute{Namespace: "", Key: "class", Val: "form-control"}, vugu.VGAttribute{Namespace: "", Key: "style", Val: "height:40vh; resize:none;"}, vugu.VGAttribute{Namespace: "", Key: "readonly", Val: ""}}}
 				parent.AppendChild(n)
 				n.InnerHTML = fmt.Sprint(data.Result)
 				n = &vugu.VGNode{Type: vugu.VGNodeType(1), Data: "\n        ", DataAtom: vugu.VGAtom(0), Namespace: "", Attr: []vugu.VGAttribute(nil)}
+				parent.AppendChild(n)
+			}
+			n = &vugu.VGNode{Type: vugu.VGNodeType(1), Data: "\n        ", DataAtom: vugu.VGAtom(0), Namespace: "", Attr: []vugu.VGAttribute(nil)}
+			parent.AppendChild(n)
+			n = &vugu.VGNode{Type: vugu.VGNodeType(3), Data: "button", DataAtom: vugu.VGAtom(102662), Namespace: "", Attr: []vugu.VGAttribute{vugu.VGAttribute{Namespace: "", Key: "type", Val: "button"}, vugu.VGAttribute{Namespace: "", Key: "class", Val: "btn btn-primary"}}}
+			parent.AppendChild(n)
+			// @click = { data.Copy() }
+			{
+				var i_ interface{} = data
+				idat_ := reflect.ValueOf(&i_).Elem().InterfaceData()
+				var i2_ interface{} = data.Copy
+				i2dat_ := reflect.ValueOf(&i2_).Elem().InterfaceData()
+				n.SetDOMEventHandler("click", vugu.DOMEventHandler{
+					ReceiverAndMethodHash: uint64(idat_[0]) ^ uint64(idat_[1]) ^ uint64(i2dat_[0]) ^ uint64(i2dat_[1]),
+					Method:                reflect.ValueOf(data).MethodByName("Copy"),
+					Args:                  []interface{}{},
+				})
+			}
+			if false {
+				// force compiler to check arguments for type safety
+				data.Copy()
+			}
+			{
+				parent := n
+				n = &vugu.VGNode{Type: vugu.VGNodeType(1), Data: "\n            Copy\n        ", DataAtom: vugu.VGAtom(0), Namespace: "", Attr: []vugu.VGAttribute(nil)}
 				parent.AppendChild(n)
 			}
 			n = &vugu.VGNode{Type: vugu.VGNodeType(1), Data: "\n    ", DataAtom: vugu.VGAtom(0), Namespace: "", Attr: []vugu.VGAttribute(nil)}
